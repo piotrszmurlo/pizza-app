13b984a0dd4d5c626a3d07c81c38a9cc5db2badf
matikurcze00 <m.roszkowski00@gmail.com>
2021-05-18 19:36:03 +0200

    admin + profile

--- react/src/components/AdminComponent.jsx
+++ react/src/components/AdminComponent.jsx
@@ -2,11 +2,41 @@ import React, {Component} from 'react'
 
 class AdminComponent extends Component {
 
+  constructor(props){
+    super(props);
+    this.sendOrder=this.sendOrder.bind(this);
+  }
 
   render(){
     return(
-    <div>
-      <h1>Hey</h1>
+      <div>
+          {this.state.orders.map(
+            order =>
+            <tr key={order.id}>
+              <div className="container">
+    {this.state.message &&<div className="alert alert-success">{this.state.message}</div>}
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Order number</th>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <td>{order.number}</td>
+        <td>{order.name}</td>
+        <td>{order.address}</td>
+        <td>${order.total}</td>
+        <th>
+        <button className="btn btn-danger" onClick={() => this.sendOrder}>Done</button>
+        </th>
+      </tbody>
+      </table>
+      </div>
+      </tr>
+          )}
     </div>
     )}
 }
--- restful-web-services/src/main/java/com/pap/rest/webservices/restfulwebservices/pizzaapp/OrderHistoryResource.java
+++ restful-web-services/src/main/java/com/pap/rest/webservices/restfulwebservices/pizzaapp/OrderHistoryResource.java
@@ -1,5 +1,7 @@
 package com.pap.rest.webservices.restfulwebservices.pizzaapp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
